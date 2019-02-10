408cd26404513b6577a0328fd2e022a6e2e7aa86
Alastair Tse <alastair@liquidx.net>
2019-02-10 16:17:56 +0900

    Added line plot and zoomable feature.

--- app.js
+++ app.js
@@ -1,48 +1,107 @@
 // import gpxparse from "gpx-parse";
 import * as d3 from "d3";
 
-function plot(gpxObject) {
-  let points = [];  // lat, lon, elevation, time (2019-01-16T01:40:28.710Z)
+var getPoints = function(gpxObject) {
+  var points = [];  // lat, lon, elevation, time (2019-01-16T01:40:28.710Z)
   for (var track of gpxObject.tracks) {
     console.log(`-- Track ${track.name}`);
     for (var segment of track.segments) {
       console.log(`  -- Segment`);
       points = segment;
-      console.log(points[0]);
     }
   }
+  return points;
+}
 
-  var width = 240;
-  var height = 240;
-  var barWidth = 1;
+function plot(gpxObject) {
+  var points = getPoints(gpxObject);
+
+  const width = 240;
+  const height = 240;
+  const barWidth = 1;
 
   var datetimeParse = d3.isoParse;
 
+  points = points.map(p => { return {lat: p.lat, lon: p.lon, elevation: p.elevation, time: datetimeParse(p.time)} });
+
+  const visualExtent = [[0, 0], [width, height]];
+  const xExtent = [0, width];
+  const yExtent = [0, height];
+
   var x = d3.scaleTime()
-    .domain([d3.min(points, d => { return datetimeParse(d.time)}),
-             d3.max(points, d => { return datetimeParse(d.time)})])
-    .range([0, width]);
+    .domain(d3.extent(points, d => d.time))
+    .range(xExtent);
 
   var y = d3.scaleLinear()
-    .domain([d3.min(points, d => d.elevation), d3.max(points, d => d.elevation)])
-    .range([0, height]);
+    .domain(d3.extent(points, d => d.elevation))
+    .range(yExtent);
 
-  var chart = d3.select('#chart1')
+  var chart1 = d3.select('#chart1')
     .attr('height', height)
     .attr('width', width);
 
-  var bar = chart.selectAll('g')
+  var area = d3.area()
+      .x(d => x(d.time))
+      .y0(height)
+      .y1(d => y(d.elevation));
+
+  var line = d3.line()
+      .defined(d => !isNaN(d.time))
+      .x(d => x(d.time))
+      .y(d => { return height - y(d.elevation); });  // svg's (0, 0) starts from the top.
+
+  // chart.append('path')
+  //     .attr('class', 'area')
+  //     .attr('d', d => { return area(d) });
+
+  // line chart
+
+  chart1.append('path')
+    .datum(points)
+    .attr('class', 'line')
+    .attr("fill", "none")
+    .attr("stroke", "white")
+    .attr("stroke-width", 1.5)
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-linecap", "round")    
+    .attr('d', d => line(d));
+
+  // bar chart
+
+  var chart2 = d3.select('#chart2')
+    .attr('height', height)
+    .attr('width', width);
+  
+  var bar = chart2.selectAll('g')
     .data(points)
     .enter().append('g')
       .attr('transform', function(d, i) { 
-        var xs = x(datetimeParse(d.time));
+        var xs = x(d.time);
         return `translate(${xs * barWidth}, 0)`
       });
-
   bar.append('rect')
     .attr('width', barWidth)
     .attr('height', d => y(d.elevation))
     .attr('y', d => { return height - y(d.elevation)});
+
+  // zooming functionality.
+
+  var zoomed = function() {
+    var t = d3.event.transform;
+    x.range(xExtent.map(d => t.applyX(d)));
+
+    // force line to redraw.
+    var l = chart1.select('.line');
+    l.attr('d', d => line(d));
+  };
+
+  var zoom = d3.zoom()
+    .scaleExtent([1, 32])
+    .translateExtent(visualExtent)
+    .extent(visualExtent)
+    .on('zoom', zoomed);
+
+  chart1.call(zoom);
 }
 
 function main() {
