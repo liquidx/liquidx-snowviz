b7e66e499e22081e3934f87001eca3f4a10cb2b6
Alastair Tse <alastair@liquidx.net>
2019-02-11 22:58:48 +0900

    Merge

--- v/viz.js
+++
@@ -1,7 +0,0 @@
-
-var gpxUrl = "https://cdn.glitch.com/5874eab4-a913-4022-82c7-c42ab13dae78%2Fskitracks-2018-01-16a.gpx?1549628411552";
-var gpxParse = require("gpx-parse");
-
-gpxParse.parseRemoteGpxFile(gpxUrl, function(err, data) {
-    console.log(gpxUrl);
-});
